6c4272597fb384fc12112209f884304b503fc35f
leolixingyou <leeusngwoo0501@naver.com>
2025-01-24 16:58:52 +0900

    Update .ignore and modify the yolop training code with cpu

---
+++ docker/yolovp.sh
@@ -0,0 +1,11 @@
+sudo docker run -it \
+-v "$(pwd)/../":/workspace \
+-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+-e XDG_RUNTIME_DIR=/tmp/runtime-root \
+-e DISPLAY=unix$DISPLAY \
+--net=host \
+--gpus all \
+--privileged \
+--name yolovp \
+leolixingyou/yolo_secries:yolop_training
+
